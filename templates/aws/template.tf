3e88ae1f9eee0d04a923bfafe55fa4fc4c90fdbe
ryota <sakamoto.ryota@tis.co.jp>
2016-02-05 14:43:47 +0900

    Refactoring variables

--- templates/aws/template.tf
+++ templates/aws/template.tf
@@ -1,38 +1,3 @@
-variable "vpc_id" {
-  description = "VPC ID which is created by common network pattern."
-}
-variable "subnet_ids" {
-  description = "Subnet ID which is created by common network pattern."
-}
-variable "shared_security_group" {
-  description = "SecurityGroup ID which is created by common network pattern."
-}
-variable "key_name" {
-  description = "Name of an existing EC2/OpenStack KeyPair to enable SSH access to the instances."
-}
-variable "wordpress_image" {
-  description = "[computed] Wordpress Image Id. This parameter is automatically filled by CloudConductor."
-}
-variable "wordpress_instance_type" {
-  description = "Wordpress instance type."
-  default = "t2.small"
-}
-variable "wordpress_url" {
-  description = "Wordpress url"
-}
-variable "wordpress_title" {
-  description = "Wordpress title."
-}
-variable "wordpress_admin_user" {
-  description = "Wordpress admin user name."
-}
-variable "wordpress_admin_pswd" {
-  description = "Wordpress admin user password."
-}
-variable "wordpress_admin_email" {
-  description = "Wordpress admin email address."
-}
-
 resource "template_file" "init" {
   template = "${file("${path.module}/init.tpl")}"
 
@@ -40,7 +5,7 @@ resource "template_file" "init" {
     wordpress_url = "${var.wordpress_url}"
     wordpress_title = "${var.wordpress_title}"
     wordpress_admin_user = "${var.wordpress_admin_user}"
-    wordpress_admin_pswd = "${var.wordpress_admin_pswd}"
+    wordpress_admin_password = "${var.wordpress_admin_password}"
     wordpress_admin_email = "${var.wordpress_admin_email}"
   }
 }
