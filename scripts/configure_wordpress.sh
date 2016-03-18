7106cc611bf8bee54eac570d089f1ce3b2e91b4e
Yoshimi Tominaga <tominaga.yoshimi@tis.co.jp>
2016-03-18 19:50:50 +0900

    Modify to get domain from consul kvs

--- scripts/configure_wordpress.sh
+++ scripts/configure_wordpress.sh
@@ -2,29 +2,26 @@
 set -o pipefail
 [ -f scripts/functions.sh ] && source scripts/functions.sh
 
+function get_domain() {
+  domain=`consul_kv_get "cloudconductor/system_domain" | grep -o '"dns": ".*"' | cut -d '"' -f4`
+  if [ -n "${domain}" ]; then
+    echo -n "${domain}"
+  else
+    self_address=`curl -s "http://169.254.169.254/latest/meta-data/public-ipv4"`
+    echo -n "$self_address"
+  fi
+}
+
 function wordpress_first_settings() {
   # Configure WordPress Settings
   load_cfn_parameters
-  sudo -u apache -- /usr/local/bin/wp core install --path=/var/www/wordpress --url="${WordPressUrl}" \
+  wordpress_domain=`get_domain`
+  sudo -u apache -- /usr/local/bin/wp core install --path=/var/www/wordpress --url="http://${wordpress_domain}" \
     --title="'${WordPressTitle}'" --admin_user="${WordPressAdminUser}" \
     --admin_password="${WordPressAdminPassword}" --admin_email="${WordPressAdminEmail}"
   sudo -u apache -- /usr/local/bin/wp core update --path=/var/www/wordpress
   sudo -u apache -- /usr/local/bin/wp core update-db --path=/var/www/wordpress
   sudo -u apache -- /usr/local/bin/wp plugin update --all --path=/var/www/wordpress
-}
-
-function get_domain() {
-  load_cfn_parameters
-  if [ -n "${WordPressUrl}" ]; then
-    url="${WordPressUrl}"
-    proto=`echo "${WordPressUrl}" | sed -e 's,^\(.*://\).*,\1,g'`
-    url=`echo "${WordPressUrl/$proto/}"`
-    host=`echo "$url" | cut -d/ -f1`
-    echo -n "$host"
-  else
-    self_address=`curl -s "http://169.254.169.254/latest/meta-data/public-ipv4"`
-    echo -n "$self_address"
-  fi
 }
 
 function update_apache_virtual_host() {
